e75f49a9fbec00680de52a5178ac4a39448ae9b6
Gregor <gregor@martynus.net>
2019-04-28 13:49:12 -0700

    feat: set to "dist-web/index.bundled.js" if file exists

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const { existsSync } = require("fs");
+
 exports.manifest = function manifest(manifest) {
-  manifest.unpkg = 'dist-web/index.js'
-}
+  if (existsSync("pkg/dist-web/index.bundled.js")) {
+    manifest.unpkg = "dist-web/index.bundled.js";
+    return;
+  }
+
+  if (existsSync("pkg/dist-web/index.js")) {
+    manifest.unpkg = "dist-web/index.js";
+    return;
+  }
+
+  throw new Error(
+    "[pika-plugin-unpkg-field] pkg/dist-web/index(.bundled).js does not exist"
+  );
+};
